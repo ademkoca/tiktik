ee5d3dfcedbb435812dd54475828b160aecc38dd
Adem Koca <aademkocaa@gmail.com>
2022-10-26 16:38:40 +0200

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,39 @@
+import axios from 'axios';
+import NoResults from '../components/NoResults';
+import VideoCard from '../components/VideoCard';
+import { Video } from '../types';
+import { BASE_URL } from '../utils';
+
+interface IProps {
+  videos: Video[];
+}
+
+const Home = ({ videos }: IProps) => {
+  console.log(videos);
+  return (
+    <div className="flex flex-col gap-10 videos h-full">
+      {videos.length ? (
+        videos.map((video: Video) => <VideoCard post={video} key={video._id} />)
+      ) : (
+        <NoResults text={'No Videos'} />
+      )}
+    </div>
+  );
+};
+
+export const getServerSideProps = async ({
+  query: { topic },
+}: {
+  query: { topic: string };
+}) => {
+  let response = await axios.get(`${BASE_URL}/api/post`);
+
+  if (topic) {
+    response = await axios.get(`${BASE_URL}/api/discover/${topic}`);
+  }
+
+  return {
+    props: { videos: response.data },
+  };
+};
+export default Home;
